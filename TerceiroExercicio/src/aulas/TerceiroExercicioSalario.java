b7cc38837f89c2139cc6a4619e81e1d69695f74e
Luís Felipe <luis.felipe.botelho.silva@gmail.com>
2022-07-21 13:37:59 -0300

    meu primeiro exercicio git

---
+++ TerceiroExercicio/src/aulas/TerceiroExercicioSalario.java
@@ -0,0 +1,29 @@
+package aulas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TerceiroExercicioSalario {
+
+	public static void main(String[] args) {
+		
+		double a = 0;
+		double b = 0;
+		
+		Scanner leia = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.println("Digite quanto você ganha por hora: ");
+		a = leia.nextDouble();
+		
+		System.out.println("Quantas horas você trabalha no mÊs? ");
+		b = leia.nextDouble();
+		
+		double resultado = a * b;
+		System.out.println("Seu salário no mês foi: " + resultado);
+		
+		leia.close();
+		
+
+	}
+
+}
---
+++ TerceiroExercicio/src/aulas/TerceiroExercicoNotas.java
@@ -0,0 +1,36 @@
+package aulas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TerceiroExercicoNotas {
+
+	public static void main(String[] args) {
+		
+		double a = 0;
+		double b = 0;
+		double c = 0;
+		double d = 0;
+		
+		Scanner leia = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.println("Digite a nota do primeiro bimestre: ");
+		a = leia.nextDouble();
+		
+		System.out.println("Digite a nota do segundo bimestre: ");
+		b = leia.nextDouble();
+		
+		System.out.println("Digite a nota do terceiro bimestre: ");
+		c = leia.nextDouble();
+		
+		System.out.println("Digite a nota do quarto bimestre: ");
+		d = leia.nextDouble();
+		
+		double resultado = (a + b + c + d)/4;
+		System.out.println("A média bimestral do aluno é: "+ resultado);
+		
+		leia.close();
+
+	}
+
+}
